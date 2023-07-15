d63198c5085b444596d61e4b327c2df26f926e40
banaz <banazsinjary@gmail.com>
2023-07-14 20:22:21 -0700

    getting the grid

--- src/Parks.tsx
+++ src/Parks.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "./App.css";
 
 const Parks: React.FC = () => {
   const [parks, setParks] = useState([]);
@@ -10,15 +10,15 @@ const Parks: React.FC = () => {
     const fetchParks = async () => {
       try {
         const response = await axios.get(
-          'https://developer.nps.gov/api/v1/parks?&limit=500&api_key=hm7AFz709emgcq69d0bRkSYxJ34mWehxJ2ewiMkN'
+          "https://developer.nps.gov/api/v1/parks?&limit=500&api_key=hm7AFz709emgcq69d0bRkSYxJ34mWehxJ2ewiMkN"
         );
         const nationalParks = response.data.data.filter(
-            (park: Park) => park.designation === 'National Park'
-          );
-      
-          setParks(nationalParks);
+          (park: Park) => park.designation === "National Park"
+        );
+
+        setParks(nationalParks);
       } catch (error) {
-        console.error('Error fetching parks:', error);
+        console.error("Error fetching parks:", error);
       }
     };
 
@@ -30,10 +30,15 @@ const Parks: React.FC = () => {
       <h1>National Parks</h1>
       <div className="park-grid">
         {parks.map((park: any) => (
-          <Link to={`/park/${park.parkCode}`} key={park.parkCode}>
-            <div className="park-card">
-              <h2>{park.name}</h2>
-            </div>
+          <Link
+            to={`/park/${park.parkCode}`}
+            key={park.parkCode}
+            className="park-square"
+            style={{
+              backgroundImage: `url(./assets/national-park-pics/${park.parkCode}.jpeg)`,
+            }}
+          >
+            <div className="park-card">{park.name}</div>
           </Link>
         ))}
       </div>
